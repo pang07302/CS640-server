feea798b1952fd55b8e0f4f383d48e921a01ff0a
pang07302 <pang07302@gmail.com>
2022-06-10 11:12:34 +0100

    .

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const PORT = process.env.port || 8000
 const app = express()
 const {Haptic} = require('./model/model')
+const request = require('request');
 
 const url = 'mongodb+srv://user:user@cluster0.ts2fe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
@@ -24,7 +25,16 @@ app.use(cors({
 }))
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: true, limit: '50mb'}))
-
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
 
 app.listen(PORT, () => {
     console.log(`Server listening or port ${PORT}`)
@@ -38,14 +48,23 @@ app.get("/url", (req, res, next) => {
 app.post('/addData', async (req, res)=> {
     console.log(req.body);
     let haptic = new Haptic(req.body)
-    console.log(haptic)
     await haptic.save()
     console.log('create data successfully')
     res.status(200).send(haptic)
  });
 
-app.get('/getData', async (req, res)=>{
-    console.log(',,,')
+ app.get('/getData', async (req, res)=>{
     let haptic = await Haptic.find();
-    res.send(haptic)
+    res.send(haptic)     
 });
+
+// app.get('/getData-proxy', async (req, res)=>{
+//     request(
+//         { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+//         async (error, response, body) => {
+//             console.log(',,,')
+//             let haptic = await Haptic.find();
+//             res.send(haptic)
+//         }
+//     )        
+// });
