803a34f22f9171342037ecb564a01122fd4ff363
pang07302 <pang07302@gmail.com>
2022-06-10 11:17:41 +0100

    .

--- server.js
+++ server.js
@@ -25,16 +25,17 @@ app.use(cors({
 }))
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: true, limit: '50mb'}))
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','*');
+//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+//     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+//     next(); 
+// })
+app.use((req, res, next) => {
+    
+    res.header('Access-Control-Allow-Origin', 'http://192.168.1.3:3000', 'http://192.168.1.10:3000');
+    next();
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening or port ${PORT}`)
