326a726b54d818dc023093ff673873ad29c1068e
pang07302 <pang07302@gmail.com>
2022-06-11 10:52:34 +0100

    .

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    // console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+})
+
 
 const url = 'mongodb+srv://user:user@cluster0.ts2fe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
